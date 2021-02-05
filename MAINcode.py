796b08f6663d4a5511d9806197170c3216a4f306
Eli Pacheco <eli.pacheco55@gmail.com>
2021-02-04 17:27:17 -0700

    This adds a bit of fun

---
+++ MAINcode.py
@@ -0,0 +1,8 @@
+
+
+print("This progam will take fahrenheight and change it to celcius. ")
+c = int(input("Input the degrees in fahrenheight: "))
+f = (c-32)*0.5555555555
+print("The degrees in celcius is: ")
+print(f)
+print("OMG I'M DOING THE GIT STUFF")
