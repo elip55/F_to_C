878ecd77fa34181b9fbf3a7e7a06b81ff326face
Eli Pacheco <eli.pacheco55@gmail.com>
2021-03-12 16:43:17 -0700

    messing aroung with packages

---
+++ src/calc.py
@@ -0,0 +1,6 @@
+
+
+def calc():
+    fahrenheight = float(input("Input the degrees in fahrenheight: "))
+    celcius = round((fahrenheight-32)*0.5555555555, 2)
+    print(f"The degrees in celcius is: {celcius} ")
