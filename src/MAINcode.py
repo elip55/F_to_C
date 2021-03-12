f794b18480fd53a88aa0037156456fd8b254c997
Eli Pacheco <77797720+elip55@users.noreply.github.com>
2021-03-12 16:54:52 -0700

    Delete MAINcode.py

--- src/MAINcode.py
+++
@@ -1,6 +0,0 @@
-
-
-print("This progam will take fahrenheight and change it to celcius. ")
-c = float(input("Input the degrees in fahrenheight: "))
-f = round((c-32)*0.5555555555, 2)
-print(f"The degrees in celcius is: {f} ")
